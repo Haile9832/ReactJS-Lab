0923a1da4a9e62e379fe253b3148999e5c6f0915
Your Name <you@example.com>
2022-07-11 15:57:31 +0700

    lab03.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent';
 import './App.css';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
           <NavbarBrand href='/'>Hello ae</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
